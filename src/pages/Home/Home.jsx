55b3afef57aa4553e549f29deb7cfe7517fd3505
NahuelArg <claudioarganaraz86@gmail.com>
2025-01-22 02:19:19 -0300

    style: Refactor homepage styles and update profile image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,34 @@
 import React from "react";
-import { useTranslation } from 'react-i18next'; // Importa el hook
-import "./Home.css"; // Importar el archivo CSS
-import perfilImage from "../../assets/perfilName.jpeg";
+
+import { useTranslation } from 'react-i18next';
 import Linkedin from "../../components/Linkedin.jsx";
 import Github from "../../components/Github.jsx";
 import Whatsapp from "../../components/Whatsapp.jsx";
+import "./Home.css"; // Importar el archivo CSS para Home
+import PerfilImage from  '../../../public/PerfilImage.png'
+import Navbar from "../../components/NavBar/NavBar.jsx";
+
 
 const Home = () => {
-  const { t } = useTranslation(); // Obtén la función t() para traducir
+  const { t } = useTranslation(); // Obtener traducciones
+  
 
   return (
     <section className="section-container">
+      {/* Componente Navbar con la lógica de visibilidad */}
+      <Navbar />
+
+      {/* Contenido principal de la página */}
       <div className="home" data-aos="flip-down">
         <div className="home-container">
           <div className="home-principal">
             <div className="home-image">
               <img
-                src={perfilImage} // Cambia esta ruta a la imagen que quieras usar
+                
+                src={PerfilImage}
                 alt={t('home.profileAltText')}
+              
+              
               />
             </div>
             <div>
@@ -40,20 +51,12 @@ const Home = () => {
                 </a>
                 <a
                   className="icon"
-                  href="https://wa.me/qr/SKAVVYZHLGJEB1"
+                  href="https://wa.me/qr/SKAVVYZHLGJJEB1"
                 >
                   <Whatsapp />
                 </a>
               </div>
             </div>
-          </div>
-          <div className="home-buttons">
-            <a href="#projects" className="button">
-              {t('home.viewProjects')}
-            </a>
-            <a href="#contact" className="button">
-              {t('home.contactMe')}
-            </a>
           </div>
         </div>
       </div>
